19df5754db1eddb4a608259cbd37fd87c7f93652
HongJinBom <fasdgoc@gmail.com>
2016-03-27 14:59:50 +0900

    editt

--- 04/ClickGame/GameManager.js
+++ 04/ClickGame/GameManager.js
@@ -1,54 +1,20 @@
-/////////Global Variable
-
-/* Game View Element */
-var gameScreen = document.getElementById('game-screen');
-/* Game View info */
-var scrWidth = gameScreen.clientWidth;
-var scrHeight = gameScreen.clientHeight;
-
-/* Game state element */
-var score = document.getElementById('score');
-var level = document.getElementById('level');
-var miss = document.getElementById('miss');
-var chance = document.getElementById('chance');
-/* Game State Data */
-var bGameStart = true;
-var curScore = 0;
-var curLevel = 1;
-var curMiss = 0;
-var curChance = 11;
-var nInterval = 1500;
-
-
-
-/**************** Game Manager Object ****************/
 function GameManager(){
-    
-    //private
-    /* member: Moving Box */
+
     var clickMe = new movingBox();
+    var timerId = null;
     
-    
-    //public
-    /* Game Start Function */
-    this.start = function(){
-        
-        // Initialize Game State
-        (function(){
-            score.innerHTML = curScore;
-            level.innerHTML = curLevel;
-            miss.innerHTML = curMiss;
-            chance.innerHTML = curChance;
-        })();
-        
-        
-        // Check Game State
-        var checkGameState = (function(){
-            //be needed SCORE to pass the current level.
+    var initializeGameState = function(){
+        score.innerHTML = curScore;
+        level.innerHTML = curLevel;
+        miss.innerHTML = curMiss;
+        chance.innerHTML = curChance;
+    };
+    var checkGameState = (function(){
+            //be needed SCORE to pass.
             var needScore = 8;
             
             return function(){
-                /* Game End Check */
+                // Is Game Ended?
                 if(curMiss >= curChance)
                 {
                     bGameStart = false;
@@ -60,7 +26,8 @@ function GameManager(){
                         gameScreen.innerHTML = 'Game End';
                     }, 1000);
                 }
-                /* Next Level Check */
+                
+                //Go to Next Level?
                 if(curScore >= needScore){
                     curLevel += 1;
                     level.innerHTML = curLevel;
@@ -76,15 +43,15 @@ function GameManager(){
                 }
             };
         })();
+    
+    /* Game Start Function */
+    this.start = function(){
         
-        /* On timer callback function */
-        var timerId = setInterval(function(){
-            
-            //1. change box position
+        initializeGameState();  
+          
+        timerId = setInterval(function(){
             clickMe.changePos();
-            //2. check "Game state"
             checkGameState();
-            
         }, nInterval);
         
         clickMe.initRect(bGameStart);
